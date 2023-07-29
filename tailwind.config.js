18415df2c46829d42d7781c4bf12441dcb92f634
Shivam77607 <shivamsharma77607@gmail.com>
2023-07-29 10:18:59 +0530

    refactor: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
