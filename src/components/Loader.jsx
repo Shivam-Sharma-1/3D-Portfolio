6143e0df63ff7304f67d518703f7b978ec60ea52
Shivam77607 <shivamsharma77607@gmail.com>
2023-08-12 18:50:31 +0530

    feat: add canvas loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,25 @@
-const Loader = () => {
-	return <div>Loader</div>;
+import { Html, useProgress } from "@react-three/drei";
+
+const CanvasLoader = () => {
+	const { progress } = useProgress();
+
+	return (
+		<Html
+			as="div"
+			center
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				flexDirection: "column"
+			}}
+		>
+			<span className="canvas-loader"></span>
+			<p className="text-[14px] text-white font-bold mt-10">
+				{progress.toFixed(2)}%
+			</p>
+		</Html>
+	);
 };
 
-export default Loader;
+export default CanvasLoader;
