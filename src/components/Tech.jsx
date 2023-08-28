cc93c0b6cbcd39d498495fba9e7715c6610a70d5
Shivam77607 <shivamsharma77607@gmail.com>
2023-08-28 17:26:18 +0530

    feat: add tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,17 @@
-const Tech = () => {
-	return <div>Tech</div>;
-};
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
-export default Tech;
+function Tech() {
+	return (
+		<div className="flex flex-row flex-wrap justify-center gap-10">
+			{technologies.map((technology) => (
+				<div className="w-28 h-28" key={technology.name}>
+					<BallCanvas icon={technology.icon} />
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default SectionWrapper(Tech, "tech");
