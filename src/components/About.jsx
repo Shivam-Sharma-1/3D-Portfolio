9e16bb9b1c7dd7d0192467db3f976dd22edd50b1
Shivam77607 <shivamsharma77607@gmail.com>
2023-09-16 17:40:33 +0530

    chore: add assets

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,7 +26,7 @@ const ServiceCard = ({ index, title, icon }) => (
 					className="w-16 h-16 object-contain"
 				/>
 
-				<h3 className="text-white text-[20px] font-bold text-center">
+				<h3 className="text-white text-[20px] font-bold text-center w-[80%]">
 					{title}
 				</h3>
 			</div>
@@ -46,15 +46,21 @@ const About = () => {
 				variants={fadeIn("", "", 0.1, 1)}
 				className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
 			>
-				I&apos;m a skilled software developer with experience in
-				TypeScript and JavaScript, and expertise in frameworks like
-				React, Node.js, and Three.js. I&apos;m a quick learner and
-				collaborate closely with clients to create efficient, scalable,
-				and user-friendly solutions that solve real-world problems.
-				Let&apos;s work together to bring your ideas to life!
+				Hello! I&apos;m Shivam Sharma, a passionate web developer and
+				problem solver. Currently, I&apos;m the Tech Team Lead at GDSC
+				MVJCE, and I&apos;m pursuing a B.E. degree in Computer Science
+				and Engineering at MVJ College Of Engineering in Bengaluru,
+				India, with an expected graduation year of 2025. I&apos;m a
+				software developer with experience in TypeScript, TailwindCSS,
+				and JavaScript. I specialize in frameworks such as React,
+				Node.js, Next.js, React-Fiber, and Three.js. My knowledge
+				extends to fundamental data structures and algorithms, making me
+				well-versed in frontend web development.
+				<br className="sm:block hidden" />
+				Let&apos;s collaborate to bring your ideas to life!
 			</motion.p>
 
-			<div className="mt-20 flex flex-wrap gap-10 justify-center">
+			<div className="mt-20 flex flex-wrap gap-10">
 				{services.map((service, index) => (
 					<ServiceCard
 						key={service.title}
