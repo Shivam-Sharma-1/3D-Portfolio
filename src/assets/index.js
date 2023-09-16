764c998273d2c2d67d85c53a813f16f53ec0d5ba
Shivam77607 <shivamsharma77607@gmail.com>
2023-09-16 17:42:13 +0530

    chore: add new assets imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -19,44 +19,66 @@ import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import threejs from "./tech/threejs.png";
+import expressjs from "./tech/expressjs.png";
+import nextjs from "./tech/nextjs.png";
+import firebase from "./tech/firebase.png";
+import nextauthjs from "./tech/nextauthjs.png";
+import prisma from "./tech/prisma.png";
+import c from "./tech/c.png";
+import java from "./tech/java.png";
+import python from "./tech/python.png";
+import framerMotion from "./tech/framer-motion.png";
+import postman from "./tech/postman.png";
+import styledComponents from "./tech/styled-components.png";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import spookyRun from "./projects/spooky-run.png";
+import netflixClone from "./projects/netflix-clone.png";
+import vanLife from "./projects/vanlife.png";
 
 export {
-  logo,
-  backend,
-  creator,
-  mobile,
-  web,
-  github,
-  menu,
-  close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
+	logo,
+	backend,
+	creator,
+	mobile,
+	web,
+	github,
+	menu,
+	close,
+	css,
+	docker,
+	figma,
+	git,
+	html,
+	javascript,
+	mongodb,
+	nodejs,
+	reactjs,
+	redux,
+	tailwind,
+	typescript,
+	threejs,
+	meta,
+	shopify,
+	starbucks,
+	tesla,
+	nextjs,
+	styledComponents,
+	expressjs,
+	firebase,
+	nextauthjs,
+	prisma,
+	c,
+	java,
+	python,
+	framerMotion,
+	postman,
+	spookyRun,
+	netflixClone,
+	vanLife
 };
