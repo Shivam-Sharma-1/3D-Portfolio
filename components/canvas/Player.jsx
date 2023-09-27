2c71f21fc9e52cdc622ddcacbd626a96fe82e042
Shivam77607 <shivamsharma77607@gmail.com>
2023-09-27 09:59:05 +0530

    feat: optimise model

--- components/canvas/Player.jsx
+++ components/canvas/Player.jsx
@@ -11,6 +11,7 @@ import { Suspense, useEffect, useLayoutEffect, useRef, useState } from "react";
 import CanvasLoader from "../Loader";
 import { useFrame } from "@react-three/fiber";
 import { Vector3 } from "three";
+import PlayerModel from "./models/PlayerModel";
 
 function Player({ playerref, isMobile }) {
 	const group = useRef();
@@ -64,101 +65,14 @@ function Player({ playerref, isMobile }) {
 				autoRotate={false}
 			/>
 			<Suspense fallback={<CanvasLoader />}>
-				<mesh>
-					<group
-						ref={group}
-						dispose={null}
-						rotation={[-1.6, 0, 0]}
-						scale={2}
-						position={[0, -1.6, 0]}
-					>
-						<group>
-							<primitive object={nodes.Hips} />
-							<skinnedMesh
-								frustumCulled={false}
-								geometry={nodes.Wolf3D_Body.geometry}
-								material={materials.Wolf3D_Body}
-								skeleton={nodes.Wolf3D_Body.skeleton}
-							/>
-							<skinnedMesh
-								frustumCulled={false}
-								geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
-								material={materials.Wolf3D_Outfit_Bottom}
-								skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
-							/>
-							<skinnedMesh
-								frustumCulled={false}
-								geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
-								material={materials.Wolf3D_Outfit_Footwear}
-								skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
-							/>
-							<skinnedMesh
-								frustumCulled={false}
-								geometry={nodes.Wolf3D_Outfit_Top.geometry}
-								material={materials.Wolf3D_Outfit_Top}
-								skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
-							/>
-							<skinnedMesh
-								frustumCulled={false}
-								geometry={nodes.Wolf3D_Hair.geometry}
-								material={materials.Wolf3D_Hair}
-								skeleton={nodes.Wolf3D_Hair.skeleton}
-							/>
-							<skinnedMesh
-								frustumCulled={false}
-								name="EyeLeft"
-								geometry={nodes.EyeLeft.geometry}
-								material={materials.Wolf3D_Eye}
-								skeleton={nodes.EyeLeft.skeleton}
-								morphTargetDictionary={
-									nodes.EyeLeft.morphTargetDictionary
-								}
-								morphTargetInfluences={
-									nodes.EyeLeft.morphTargetInfluences
-								}
-							/>
-							<skinnedMesh
-								frustumCulled={false}
-								name="EyeRight"
-								geometry={nodes.EyeRight.geometry}
-								material={materials.Wolf3D_Eye}
-								skeleton={nodes.EyeRight.skeleton}
-								morphTargetDictionary={
-									nodes.EyeRight.morphTargetDictionary
-								}
-								morphTargetInfluences={
-									nodes.EyeRight.morphTargetInfluences
-								}
-							/>
-							<skinnedMesh
-								frustumCulled={false}
-								name="Wolf3D_Head"
-								geometry={nodes.Wolf3D_Head.geometry}
-								material={materials.Wolf3D_Skin}
-								skeleton={nodes.Wolf3D_Head.skeleton}
-								morphTargetDictionary={
-									nodes.Wolf3D_Head.morphTargetDictionary
-								}
-								morphTargetInfluences={
-									nodes.Wolf3D_Head.morphTargetInfluences
-								}
-							/>
-							<skinnedMesh
-								frustumCulled={false}
-								name="Wolf3D_Teeth"
-								geometry={nodes.Wolf3D_Teeth.geometry}
-								material={materials.Wolf3D_Teeth}
-								skeleton={nodes.Wolf3D_Teeth.skeleton}
-								morphTargetDictionary={
-									nodes.Wolf3D_Teeth.morphTargetDictionary
-								}
-								morphTargetInfluences={
-									nodes.Wolf3D_Teeth.morphTargetInfluences
-								}
-							/>
-						</group>
-					</group>
-				</mesh>
+				<PlayerModel
+					nodes={nodes}
+					materials={materials}
+					rotation={[-1.6, 0, 0]}
+					position={[0, -1.6, 0]}
+					scale={2}
+					group={group}
+				/>
 			</Suspense>
 		</View>
 	);
