f65653972447cb887d748b67861f6f0bcedb90fb
Shivam77607 <shivamsharma77607@gmail.com>
2024-04-30 18:05:18 +0530

    Refactor: Update Tech component styles and layout

--- components/Tech.jsx
+++ components/Tech.jsx
@@ -67,7 +67,7 @@ function Tech() {
   ));
 
   return (
-    <section className="w-full h-fit p-8" id="skills">
+    <section className="w-full h-fit p-8 mt-20" id="skills">
       <motion.div
         variants={textVariant()}
         initial="hidden"
