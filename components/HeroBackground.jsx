d220f454cf1bfdd3cade1ee1161a6b87241003d1
Shivam-Sharma-1 <shivamsharma77607@gmail.com>
2026-08-02 12:05:07 +0530

    feat: add preserveAspectRatio attribute to mountain components and update mountain5.svg dimensions

--- components/HeroBackground.jsx
+++ components/HeroBackground.jsx
@@ -9,8 +9,14 @@ import HeroSvg from "../public/assets/background/hero-wave.svg";
 function HeroBackground() {
   return (
     <div className="absolute top-0 w-full h-[100svh] bg-gradient-to-b from-opacity-[6.33%] dark:from-[#322d6d] from-[#b0afb1] to-opacity-[39.13%] dark:to-[#663182] to-[#51afda]">
-      <Mountain1 className="w-full h-[446px] wave top-[170px]" />
-      <Mountain2 className="w-full h-[464px] wave top-[160px]" />
+      <Mountain1
+        preserveAspectRatio="none"
+        className="w-full h-[446px] wave top-[170px]"
+      />
+      <Mountain2
+        preserveAspectRatio="none"
+        className="w-full h-[464px] wave top-[160px]"
+      />
       <div className="w-full h-[1503px] wave top-[-200px]">
         <Image
           src={"/assets/background/blur-layer.png"}
@@ -18,9 +24,18 @@ function HeroBackground() {
           fill={true}
         />
       </div>
-      <Mountain3 className="w-full h-[408px] wave top-[347px]" />
-      <Mountain5 className="w-full h-[867px] wave md:top-[450px] top-[300px]" />
-      <HeroSvg className="md:w-full w-full h-[1200px] wave md:top-[100dvh] md:left-0 top-[520px] dark:text-[#181826] text-[#4e2f84]" />
+      <Mountain3
+        preserveAspectRatio="none"
+        className="w-full h-[408px] wave top-[347px]"
+      />
+      <Mountain5
+        preserveAspectRatio="none"
+        className="w-full h-[867px] wave md:top-[450px] top-[300px]"
+      />
+      <HeroSvg
+        preserveAspectRatio="none"
+        className="md:w-full w-full h-[1200px] wave md:top-[100dvh] md:left-0 top-[520px] dark:text-[#181826] text-[#4e2f84]"
+      />
     </div>
   );
 }
